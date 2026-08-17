a7abb2bb14b33134bdc39ebb05d9c920cffdece0
carloscastrofilho <cpcftq@gmail.com>
2026-08-17 08:47:35 -0300

    aula-10-08-ok

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,66 +1,66 @@
-
-import { Text, View, StyleSheet, TextInput, Button, Image } from "react-native";
+import { Button, Image, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function Index() {
   return (
-    <View style={{flex: 1, padding: 5, gap: 5, backgroundColor:"#fff"}}>
-    <View style={styles.container}>
-      <Image source={require("@/assets/images/favicon.png")} />
-      <Text style={styles.titulo}>Login</Text>
-      <Text style={{fontSize:16, fontWeight: "bold"}}>Aula 10/08/2026</Text>
-    </View>
-    <View style={styles.main}>
-      
-      <Text style={styles.inputText}>usuário:</Text>
-      <TextInput style={styles.input}/>
+    <View style={{ flex: 1, padding: 5, gap: 5, backgroundColor: "#fff" }}>
+      <View style={styles.container}>
+        <Image source={require("@/assets/images/favicon.png")} />
+        <Text style={styles.titulo}>Login</Text>
+        <Text style={{ fontSize: 16, fontWeight: "bold" }}>
+          Aula 10/08/2026
+        </Text>
+      </View>
+      <View style={styles.main}>
+        <Text style={styles.inputText}>usuário:</Text>
+        <TextInput style={styles.input} />
 
-      <Text style={styles.inputText}>senha:</Text>
-      <TextInput style={styles.input}/>
-
-    </View>
-     <View style={styles.footer}>
-      
-      <Text style={styles.inputText}>Não tem login, faça o cadastro:</Text>
-      <Button title="Acessar" />
-
-    </View>
+        <Text style={styles.inputText}>senha:</Text>
+        <TextInput style={styles.input} />
+      </View>
+      <View style={styles.footer}>
+        <Text style={styles.inputText}>
+          Não tem login, faça o cadastro aqui!
+        </Text>
+        <Button title="Acessar" />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1/3,
+    flex: 1 / 3,
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "lightgreen",
   },
-  titulo : {
-    color: 'red'
+  titulo: {
+    color: "#000",
+    fontSize: 28,
+    fontWeight: 600,
   },
 
   main: {
-    flex: 1/3,
+    flex: 1 / 3,
     alignItems: "center",
     justifyContent: "center",
-    
   },
-  input :{
+  input: {
     width: "100%",
-    height:36,
-    backgroundColor : "#999",
+    height: 36,
+    backgroundColor: "#999",
     marginBottom: 15,
     borderRadius: 10,
   },
-  inputText : {
+  inputText: {
     color: "#000",
     fontWeight: "500",
     fontSize: 12,
     width: "100%",
     marginBottom: 5,
   },
-    footer: {
-    flex: 1/3,
+  footer: {
+    flex: 1 / 3,
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "lightgreen",
